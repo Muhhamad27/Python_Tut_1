63098a0cc8361abbcafc7343f4da767073d04d4e
Магомед <163691844+Muhhamad27@users.noreply.github.com>
2024-12-18 01:47:18 +0300

    Update module_2_1_5.py

--- module_2_1_5.py
+++ module_2_1_5.py
@@ -1,12 +1,14 @@
 def get_password(n):
     password = ""
-
-    for i in range(1, 21):
-        for j in range(i + 1, 21):
-            if (i + j) % n == 0:
+    for i in range(1, n+1):
+        for j in range(i+1, n+1):  
+            if n % (i + j) == 0: 
                 password += str(i) + str(j)
-
     return password
+
 n = int(input("Введите число от 3 до 20: "))
-password = get_password(n)
-print(f"Пароль для числа {n}: {password}")
+if 3 <= n <= 20:
+    result = get_password(n)
+    print(f"Пароль для числа {n}: {result}")
+else:
+    print("Число должно быть в диапазоне от 3 до 20!")
